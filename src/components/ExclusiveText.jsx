ecaa79ac029988805d4faa120ebeeb59e7202939
MMW <null.mmw@gmail.com>
2023-07-07 18:08:57 -0700

    Components Added

---
+++ src/components/ExclusiveText.jsx
@@ -0,0 +1,20 @@
+export default function ExclusiveText() {
+  return (
+    <div className="lg:grid lg:grid-cols-12 lg:gap-8">
+      <div className="px-4 sm:px-6 sm:text-center md:mx-auto md:max-w-2xl lg:col-span-6 lg:flex lg:items-center lg:text-left">
+        <div>
+          <h1 className="mt-4 text-center text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl">
+            EXCLUSIVE ACCESS TO <span className="text-[#26E2FF]">333</span>
+            MEMBERS
+          </h1>
+          <p className="mt-3 text-center text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry’s standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
